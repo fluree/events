6f26b1da915af37b54ca9bb22051e76056d23563
Brian Platz <bplatz@gmail.com>
2020-12-01 14:05:03 -0500

    Use Instant, cron-utils and trap more errors

--- src/fluree/events/core.clj
+++ src/fluree/events/core.clj
@@ -8,7 +8,9 @@
             [clojure.walk :refer [keywordize-keys]]
             [clojure.tools.logging :as log]
             [cheshire.core :as json]
-            [aleph.http :as xhttp]))
+            [aleph.http :as xhttp]
+            [fluree.events.instant :as inst]
+            [fluree.events.cron :as cron]))
 
 (def system (atom {}))
 (def loop-timeout-ms 1000)
@@ -37,10 +39,17 @@
 (defn to-invoke
   "Filters schedules such that only ones that should be run are left."
   [monitor-map ref-time now]
-  (let [{:keys [eventMonitor/cron]} monitor-map
-        next-run (next-date ref-time cron)]
-    ;; if next-run is -not- after now return true (run)
-    (not (t/after? next-run now))))
+  (try
+    (let [{:keys [eventMonitor/cron]} monitor-map
+          next-run (cron/next-execution cron ref-time)]
+      ;; if next-run is -not- after now return true (run)
+      (not (inst/after? next-run now)))
+    (catch Exception e
+      (log/error e (str "Unable to parse cron expression, skipping: " (:eventMonitor/cron monitor-map)
+                        {:monitor-map monitor-map
+                         :now         (str now)
+                         :last-time   (str ref-time)}))
+      false)))
 
 
 (defn query-change?
@@ -61,7 +70,9 @@
     (if (not= result old-result)
       (do
         (log/info "Difference triggered for:" (:eventMonitor/id monitor) (:eventMonitor/name monitor))
-        @(xhttp/get "https://l92xqqxncf.execute-api.us-east-1.amazonaws.com/Prod/hello")
+        (try @(xhttp/get (:eventMonitor/webhook monitor))
+             (catch Exception e (log/error e (str "Error executing webhook: " (:eventMonitor/webhook monitor)
+                                                  {:monitor monitor}))))
         (swap! system assoc-in [:block (:_id monitor)] block)
         true)
       (do
@@ -72,10 +83,10 @@
   []
   (async/go
     (try
-      (loop [ref-time (t/now)]                              ;; last time utilized for check
+      (loop [ref-time (inst/now)]                           ;; last time utilized for check
         (when-not (:closed? @system)
           (let [next           (async/<! (async/timeout loop-timeout-ms))
-                now            (t/now)
+                now            (inst/now)
                 db             (fdb/db (:conn @system) (:ledger @system))
                 monitorRecords (monitor-records db)
                 monitors       (->> monitorRecords
